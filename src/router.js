b66a10a94c0d706311d99ecd19561a336d73645f
sameer <kumar.sam1612@gmail.com>
2020-05-31 20:41:18 +0530

    updated

--- src/router.js
+++ src/router.js
@@ -5,19 +5,19 @@ import Home from "./views/Home.vue";
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
+  mode: "hash",
   base: process.env.BASE_URL,
   routes: [
     {
       path: "/",
       name: "home",
-      component: Home
+      component: Home,
     },
     {
       path: "/about",
       name: "about",
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
-  ]
+        import(/* webpackChunkName: "about" */ "./views/About.vue"),
+    },
+  ],
 });
